4a6d8af6fbe1eece0d2bfbdd4ef794e324654db0
Manuel Rueda <manuel.rueda.un@gmail.com>
2015-07-06 18:48:44 -0300

    Add 3 more test

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -84,4 +84,55 @@ describe('jade-autocompile module', function() {
 
   });
 
+  it('compile a more complex JADE with locals and functions', function(done) {
+    var flag;
+    runs(function() {
+      runTest.call(this, compiler, 'jade/test5.jade', 'jade/test5.html', function(err, data) {
+        expect(err).toBeNull();
+        expect(data).toEqual('<html><div><label>Holaaaa</label></div><label>33.5</label><div><span>9</span></div></html>');
+        flag = true;
+      });
+
+    });
+
+    waitsFor(function() {
+      return flag;
+    });
+
+  });
+
+  it('compile a more complex JADE with locals, functions, arrays, objects and HTML comments', function(done) {
+    var flag;
+    runs(function() {
+      runTest.call(this, compiler, 'jade/test6.jade', 'jade/test6.html', function(err, data) {
+        expect(err).toBeNull();
+        expect(data).toEqual('<html><div><label>Holaaaa</label></div><label>33.5</label><div><span>9</span><!--this comment must not \'joder\'--></div><section><span>44,Chauuu</span><span>99</span></section></html>');
+        flag = true;
+      });
+
+    });
+
+    waitsFor(function() {
+      return flag;
+    });
+
+  });
+
+  it('compile the previous test but pretty', function(done) {
+    var flag;
+    runs(function() {
+      runTest.call(this, compiler, 'jade/test6.pretty.jade', 'jade/test6.pretty.html', function(err, data) {
+        expect(err).toBeNull();
+        expect(data).toEqual('\n<html>\n  <div>\n    <label>Holaaaa</label>\n  </div>\n  <label>33.5</label>\n  <div><span>9</span>\n    <!--this comment must not \'joder\'-->\n  </div>\n  <section><span>44,Chauuu</span><span>99</span></section>\n</html>');
+        flag = true;
+      });
+
+    });
+
+    waitsFor(function() {
+      return flag;
+    });
+
+  });
+
 });
